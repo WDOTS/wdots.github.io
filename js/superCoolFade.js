68ec6fcffd21915f96dd1a80fb2f1dca9e8c5c29
Jimmy <jim.me.ryan@gmail.com>
2016-07-11 13:29:46 +0100

    google font

--- js/superCoolFade.js
+++ js/superCoolFade.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $(document).on("scroll", function handler() {
         if ($('.reason').visible(true)) {
-            $(".reason").delay(600).animate({
+            $(".reason").delay(100).animate({
                     opacity: 1,
                     top: "+=20",
                 },
