64eede1b1fbe177c1abcb7abf64f6c630ee8441a
Simon Adcock <simon.adcock@guardian.co.uk>
2016-07-29 16:11:22 +0100

    improve deploy script todos

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,16 +4,11 @@ set -e # Exit with nonzero exit code if anything fails
 SOURCE_BRANCH="integration"
 TARGET_BRANCH="master"
 
-#function doCompile {
-  # TODO: add a build script here!
-  # ./compile.sh
-#}
-
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
     # TODO: reinstate
-    # doCompile
+    # npm run build
     exit 0
 fi
 
