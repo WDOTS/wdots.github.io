d0d72ef438fa606661c0dbe6c085cc570ebeec18
Simon Adcock <simon.adcock@guardian.co.uk>
2016-07-29 15:43:23 +0100

    Stop logging

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -30,9 +30,7 @@ git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Clean out existing contents
-rm -rf out/**/* || exit 0
-
-ls -al out
+rm -rf out/* || exit 0
 
 # run our deploy script
 npm run deploy
