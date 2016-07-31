14778c85def8ef56d94f7bbdd82cb2b401bc2116
SiAdcock <simonadcock2@gmail.com>
2016-07-31 12:36:42 +0100

    serve local fonts in dev mode

--- Gulpfile.js
+++ Gulpfile.js
@@ -21,7 +21,7 @@ gulp.task('serve', ['build'], () => {
     gulp.watch('src/js/*.js', ['build:js']);
     gulp.watch('src/templates/*.html', ['build:html']);
 });
-gulp.task('build', ['clean', 'build:html', 'build:js', 'build:css']);
+gulp.task('build', ['clean', 'build:html', 'build:js', 'build:css', 'build:fonts']);
 gulp.task('clean', () => {
     shell.rm('-rf', 'build');
     shell.rm('-f', 'index.html');
@@ -64,13 +64,12 @@ gulp.task('build:html', () => {
             jqueryVisible: 'build/js/vendor/jquery.visible.min.js',
             bootstrap: 'build/css/vendor/bootstrap.min.css',
             addToCalendarCss: 'build/css/vendor/atc-style-blue.css',
-            dosis: 'https://fonts.googleapis.com/css?family=Dosis',
-            html5Shiv: 'https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js',
-            respond: 'https://oss.maxcdn.com/respond/1.4.2/respond.min.js',
+            dosis: 'build/css/vendor/dosis.css',
+            html5Shiv: 'build/js/vendor/html5shiv.min.js',
+            respond: 'build/js/vendor/respond.min.js',
             addToCalendarJs: 'build/js/vendor/atc.min.js'
         };
-    }
-    else {
+    } else {
         data.vendorPaths = {
             jquery: 'https://code.jquery.com/jquery-3.1.0.min.js',
             jqueryEasing: 'https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js',
@@ -87,4 +86,10 @@ gulp.task('build:html', () => {
         .pipe(template(data))
         .pipe(browserSync.stream())
         .pipe(gulp.dest('./'));
+});
+gulp.task('build:fonts', () => {
+    if (isDev()) {
+        shell.mkdir('-p', 'build/fonts/');
+        shell.cp('src/fonts/*.woff2', 'build/fonts/');
+    }
 });
