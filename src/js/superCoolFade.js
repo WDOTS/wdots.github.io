8f6ba7f673ba93311b35157199a7a834fb28e275
Jimmy <jim.me.ryan@gmail.com>
2016-08-23 20:39:59 +0100

    refactored JS

--- src/js/superCoolFade.js
+++ src/js/superCoolFade.js
@@ -1,13 +1,35 @@
-$(document).ready(function () {
-    $(document).on('scroll', function handler() {
-        var $reasons = $('.reason');
+function fadeIntoBeing(selection, animation, callback) {
+    if (selection.visible(true)) {
+        selection.delay(600).animate(animation);
+        callback();
+    }
+}
 
-        if ($reasons.visible(true)) {
-            $reasons.delay(600).animate({
-                opacity: 1,
-                top: '+=20'
-            }, 1000, 'easeInOutBack');
+$(document).ready(function () {
+    var $arrow = $('.footer-arrow-img');
+    var $reasons = $('.reason');
+    var fade = {
+        opacity: 0.7,
+        duration: 600
+    };
+    var upAndEase = {
+        opacity: 1,
+        top: '+=20'
+    };
+    var scrollToTop = function () {
+        $('html, body').animate({ scrollTop: 0 }, 800);
+        return false;
+    };
+    $(document).on('scroll', function handler() {
+        fadeIntoBeing($arrow, fade, function callback() {
             $(document).off('scroll', handler);
-        }
+        });
     });
+    $(document).on('scroll', function handler() {
+        fadeIntoBeing($reasons, upAndEase, function callback() {
+            $(document).off('scroll', handler);
+        });
+    });
+
+    $arrow.click(scrollToTop);
 });
