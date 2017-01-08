3dd0b1e44e64866960e4223357bae8357137f9be
Simon Adcock <simonadcock2@gmail.com>
2017-01-08 19:25:33 +0000

    Revert "Register pre-cache service worker"

--- tools/tasks/generate-service-worker.js
+++
@@ -1,6 +0,0 @@
-const writeServiceWorkerFile = require('./helpers/write-service-worker-file');
-const isDev = require('./helpers/isDev');
-
-module.exports = function (callback) {
-    writeServiceWorkerFile(!isDev(), callback);
-};
