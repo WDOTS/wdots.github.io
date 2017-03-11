0c8652c8040c279f59e1d07937cffe51aee4d344
SiAdcock <simonadcock2@gmail.com>
2017-03-11 08:38:58 +0000

    lint scroll it

--- src/js/scroll-it.js
+++ src/js/scroll-it.js
@@ -1,21 +1,29 @@
 // Lightweight vanilla JS scroll helper
 // https://pawelgrzybek.com/page-scroll-in-vanilla-javascript/
+// eslint-disable-next-line no-unused-vars
 function scrollIt(element, duration, easing, callback) {
     // define timing functions (truncated, see linked article for more)
     var easings = {
-        easeInOutQuad: function(t) {
-            return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+        easeInOutQuad: function (t) {
+            return t < 0.5 ?
+                2 * t * t :
+                -1 + ((4 - (2 * t)) * t);
         }
     };
 
     // Returns document.documentElement for Chrome and Safari
     // document.body for rest of the world
-    function checkBody() {
+    var checkBody = function checkBody() {
+        var body;
+
         document.documentElement.scrollTop += 1;
-        var body = (document.documentElement.scrollTop !== 0) ? document.documentElement : document.body;
+        body = (document.documentElement.scrollTop !== 0) ?
+            document.documentElement :
+            document.body;
         document.documentElement.scrollTop -= 1;
+
         return body;
-    }
+    };
 
     var body = checkBody();
     var start = body.scrollTop;
@@ -24,9 +32,21 @@ function scrollIt(element, duration, easing, callback) {
     // Height checks to prevent requestAnimationFrame from infinitely looping
     // If the function tries to scroll below the visible document area
     // it should only scroll to the bottom of the document
-    var documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
-    var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
-    var destination = documentHeight - element.offsetTop < windowHeight ? documentHeight - windowHeight : element.offsetTop;
+    var documentHeight = Math.max(
+        document.body.scrollHeight,
+        document.body.offsetHeight,
+        document.documentElement.clientHeight,
+        document.documentElement.scrollHeight,
+        document.documentElement.offsetHeight
+    );
+    var windowHeight = (
+        window.innerHeight ||
+        document.documentElement.clientHeight ||
+        document.getElementsByTagName('body')[0].clientHeight
+    );
+    var destination = (documentHeight - element.offsetTop < windowHeight) ?
+        documentHeight - windowHeight :
+        element.offsetTop;
 
     function scroll() {
         var now = Date.now();
@@ -42,5 +62,6 @@ function scrollIt(element, duration, easing, callback) {
         }
         requestAnimationFrame(scroll);
     }
+
     scroll();
 }
