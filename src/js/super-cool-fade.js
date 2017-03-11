3873f40b99c85aed164a520edbfcd89ed90848c0
SiAdcock <simonadcock2@gmail.com>
2017-03-11 09:06:42 +0000

    css arrow fade

--- src/js/super-cool-fade.js
+++ src/js/super-cool-fade.js
@@ -5,19 +5,22 @@ function fadeIntoBeing(selection, animation, callback) {
     }
 }
 
+function classyFade(selection, classy, callback) {
+    if (selection.visible(true)) {
+        selection.addClass(classy);
+        callback();
+    }
+}
+
 $(document).ready(function () {
     var $arrow = $('.js-footer-arrow');
     var $reasons = $('.reason');
-    var fade = {
-        opacity: 0.7,
-        duration: 600
-    };
     var upAndEase = {
         opacity: 1,
         top: '+=20'
     };
     $(document).on('scroll', function handler() {
-        fadeIntoBeing($arrow, fade, function callback() {
+        classyFade($arrow, 'footer__arrow__img--visible', function () {
             $(document).off('scroll', handler);
         });
     });
