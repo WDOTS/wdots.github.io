6fc974cbf4f5ac14923ae454b100e941d0ea8734
Christopher Geary <dev@crgeary.com>
2017-03-25 16:40:56 +0000

    Added manifest.json, created gulp task, and added 3 WDOTS icon images for home screens etc

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,6 +7,7 @@ const css = require('./tools/tasks/css');
 const html = require('./tools/tasks/html');
 const fonts = require('./tools/tasks/fonts');
 const images = require('./tools/tasks/images');
+const manifest = require('./tools/tasks/manifest');
 const generateServiceWorker = require('./tools/tasks/generate-service-worker');
 
 gulp.task('default', function (done) {
@@ -20,6 +21,7 @@ gulp.task('build', function (done) {
         'build:fonts',
         'build:images',
         'generate-service-worker',
+        'build:manifest',
         'build:html',
         done
     );
@@ -29,6 +31,7 @@ gulp.task('clean', clean);
 gulp.task('build:js', js);
 gulp.task('build:css', css);
 gulp.task('build:fonts', fonts);
+gulp.task('build:manifest', manifest);
 gulp.task('build:html', html);
 gulp.task('build:images', images);
 gulp.task('generate-service-worker', generateServiceWorker);
